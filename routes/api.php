8d152b82fe5290dee52b4e16d6372546b7de2e44
May Phyusin Thant <mayphyusinthant_Offical@outlook.com>
2024-08-26 19:03:57 +0630

    Features - roles and permissions for Users CRUD and API routes implemented

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Modules\Auth\Http\Controllers\Api\AuthController;
+use App\Modules\RolesAndPermissions\Http\Controllers\Api\RolesAndPermissionsController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,10 +21,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::prefix('/v1/auth')->name('api.auth.')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         return $request->user();
     });
+});
+
+
+Route::prefix('/v1/roles_permissions')->group(function () {
+    Route::get('/', [RolesAndPermissionsController::class, 'index']);
+    Route::post('/', [RolesAndPermissionsController::class, 'store']); 
+    Route::get('/{role}', [RolesAndPermissionsController::class, 'show']); 
+    Route::put('/{role}', [RolesAndPermissionsController::class, 'update']); 
+    Route::delete('/{role}', [RolesAndPermissionsController::class, 'destroy']); 
 });
